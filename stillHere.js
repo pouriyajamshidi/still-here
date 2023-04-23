5d47cf2132489eeb8f8c353c4d487cad649f7d74
Pouriya <pouriya.jamshidi@gmail.com>
2023-04-23 14:37:58 +0200

    implement last activity time and cleanup

--- stillHere.js
+++ stillHere.js
@@ -3,41 +3,54 @@ function sleep(ms) {
 }
 
 function getDate() {
-    var d = new Date();
-    var hours = (d.getHours());
-    var minutes = (d.getMinutes());
-    var seconds = (d.getSeconds());
-    var day = (d.getDate());
-    var month = (d.getMonth());
-    var year = (d.getFullYear());
+    let d = new Date();
+    let hours = (d.getHours());
+    let minutes = (d.getMinutes());
+    let seconds = (d.getSeconds());
+    let day = (d.getDate());
+    let month = (d.getMonth());
+    let year = (d.getFullYear());
 
     return `${year}-${month}-${day} - ${hours}:${minutes}:${seconds}`;
 }
 
-function clickYes(button) {
-    button.click();
-    console.log(`${getDate()}: [Still Here] - Successfully got rid of the confirm button`)
-}
-
 function youTubeCheckDialog() {
-    const confirmButton = document.getElementById('confirm-button')
+    // const confirmButton = document.getElementById('confirm-button')
+    // const confirmButton = document.querySelector("ytd-popup-container")
 
-    if (confirmButton === null) {
-        return false;
+    // const confirmButton = document.querySelector('[aria-label="Yes"]');
+
+    // if (confirmButton === null) {
+    //     return false;
+    // }
+
+    // confirmButton.click();
+    let button = document.querySelector('button.yt-spec-button-shape-next[aria-label="Yes"]');
+
+    if (button) {
+        button.click();
+        let parent = button.parentNode;
+        parent.removeChild(button);
+        console.log(`${getDate()}: [Still Here] - Successfully got rid of the confirm button`)
     }
 
-    clickYes(confirmButton);
+    button = null;
+
     return true;
 }
 
 function youTubeMusicCheckDialog() {
-    const confirmButton = document.querySelector("body > ytmusic-app > ytmusic-popup-container > tp-yt-paper-dialog > ytmusic-you-there-renderer > div > yt-button-renderer")
+    // const confirmButton = document.querySelector("body > ytmusic-app > ytmusic-popup-container > tp-yt-paper-dialog > ytmusic-you-there-renderer > div > yt-button-renderer")
+    // const confirmButton = document.querySelector("ytmusic-popup-container")
+    // let confirmButton = document.getElementsByClassName("style-scope yt-button-renderer style-blue-text").button;
+    let button = document.getElementsByClassName("text style-scope ytmusic-you-there-renderer");
 
-    if (confirmButton === null) {
+    if (button === null) {
         return false;
     }
 
-    clickYes(confirmButton);
+    button.click();
+
     return true;
 }
 
@@ -47,12 +60,28 @@ function changeVisibility() {
     document.dispatchEvent(new Event("visibilitychange"));
 }
 
+function changeWindowLastActiveTime() {
+    window._lact = Date.now();
+}
+
+function hookEventListeners() {
+    document.addEventListener("visibilitychange", function () {
+        if (document.visibilityState === "hidden") {
+            changeVisibility();
+        }
+    });
+}
+
 async function start() {
-    var clicked = false;
+    hookEventListeners();
+    changeVisibility();
+
+    setInterval(changeWindowLastActiveTime, 180000)
+
+    let clicked = false;
 
     while (true) {
-        var site = window.location.hostname
-        changeVisibility();
+        let site = window.location.hostname;
 
         if (site === "www.youtube.com") {
             clicked = youTubeCheckDialog();
@@ -62,9 +91,10 @@ async function start() {
 
         if (clicked) {
             clicked = false;
-            await sleep(600000);
+            await sleep(60000);
         } else {
             await sleep(1000);
+            changeVisibility();
         }
     }
 }
